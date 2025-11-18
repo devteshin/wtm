e2cb517cf60ff17f023ede164b60b5ce14cf1a94
DmitryEvteshin <dmitry.evteshin@yandex.ru>
2025-11-18 20:44:58 +0400

    v2

--- src/backend/db/db.py
+++ src/backend/db/db.py
@@ -328,7 +328,7 @@ async def select_operations(conn: Connection, user_id: int, stock_id: int):
 async def select_operation(conn: Connection, user_id: int, stock_id: int, operation_id: int):
     q = """
 SELECT
-    , id
+    id
 	, doc_number
 	, doc_date
 	, SUM(net_weight) AS net_weight
@@ -342,7 +342,8 @@ WHERE
 	doc.stock = %(stock_id)s 
 	AND doc.operation = %(operation_id)s
 GROUP BY 
-	doc_number
+	id
+    , doc_number
 	, doc_date
 ORDER BY 
 	doc_date DESC
