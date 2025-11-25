df4e91ded6b0e3d281a0356a628bd6ae9d1e269d
DmitryEvteshin <dmitry.evteshin@yandex.ru>
2025-11-25 10:01:44 +0400

    v44

--- src/backend/db/db.py
+++ src/backend/db/db.py
@@ -485,12 +485,12 @@ async def update_arrival(conn: Connection, doc_id: int, doc_number: str, doc_dat
         async with conn.cursor() as cur:
             await cur.execute(q_update_doc, {"doc_id": doc_id, "doc_number": doc_number, "doc_date": doc_date})
 
-        a = 1/0
+        async with conn.cursor() as cur:
+            await cur.execute(q_delete_arrival, {"doc_id": doc_id, "material_id": material_id})
 
         #async with conn.cursor() as cur:
-        #    await cur.execute(q_delete_arrival, {"doc_id": doc_id, "material_id": material_id})
-        #async with conn.cursor() as cur:
         #    await cur.execute(q_insert_arrival, {"values_string": values_string})
+        
         async with conn.cursor() as cur:
             await cur.execute("COMMIT;")
 
