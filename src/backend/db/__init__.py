4b95012b5935d12010b3d9de042e8d72e9f67219
DmitryEvteshin <dmitry.evteshin@yandex.ru>
2026-02-24 20:29:55 +0400

    v1_002

--- src/backend/db/__init__.py
+++ src/backend/db/__init__.py
@@ -5,7 +5,7 @@ from aiomysql import DictCursor, Pool, create_pool
 
 from .db import (
     check_user, select_task, select_tasks, change_password, select_stocks, select_operations, select_operation, select_arrival, select_max_tare_id, 
-    update_job_status, check_can_login, select_tasks_progress, update_rest_gross_weight, update_arrival, delete_arrival, create_arrival,
+    update_job_status, check_can_login, select_tasks_progress, update_rest_gross_weight, update_arrival, delete_arrival, create_arrival, select_materials_meta,
     check_material_item, select_dnm_doc_number, DocumentExistsError, ItemsExistsError, ItemsConsumptionError, MaterialError
 )
 
@@ -29,6 +29,7 @@ __all__ = (
     "select_task",
     "check_user",
     "change_password",
+    "select_materials_meta",
     "select_stocks",
     "select_operations",
     "select_dnm_doc_number",
